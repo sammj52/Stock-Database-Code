adf858b44286c663fd4a92114efb8e6a1c6c540f
Jam <59035544+PumpDoctor@users.noreply.github.com>
2022-01-12 17:46:34 -0500

    Update RSI.py

--- RSI.py
+++ RSI.py
@@ -1,21 +1,28 @@
+'''14 day RSI Indicator'''
+
 import time
 import datetime
 import pandas as pd
 
 ticker = 'AAPL'
+
+'''Period 1 & 2: 15 market open days (not including the most previous market open day) to calculate the orginal upward/downward movement averages'''
 period1 = int(time.mktime(datetime.datetime(2021, 12, 15, 23, 59).timetuple()))
 period2 = int(time.mktime(datetime.datetime(2022, 1, 6, 23, 59).timetuple()))
 
+'''2nd period lists to use to calculate the Up/Down movement average (initial_up_movement*13 + previous_upchangetwo)/14)'''
 period1_listtwo = int(time.mktime(datetime.datetime(2021, 12, 16, 23, 59).timetuple()))
 period2_listtwo = int(time.mktime(datetime.datetime(2022, 1, 7, 23, 59).timetuple()))
 interval = '1d' # 1d, 1m
 
+'''Grabbing stock info'''
 query_stringone = f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={period1}&period2={period2}&interval={interval}&events=history&includeAdjustedClose=true'
 query_stringtwo = f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={period1_listtwo}&period2={period2_listtwo}&interval={interval}&events=history&includeAdjustedClose=true'
 
 df = pd.read_csv(query_stringone)
 dftwo = pd.read_csv(query_stringtwo)
 
+'''Reading just the Close columns'''
 column = df['Close'].to_string(index=False)
 columntwo = dftwo['Close'].to_string(index=False)
 
@@ -23,10 +30,12 @@ closes = column.splitlines()
 closestwo = columntwo.splitlines()
 #print(type(closes[1]))
 
+'''Turning Close columns into float values to put in list/array'''
 float_closes = list(map(float, closes))
 float_closestwo = list(map(float, closestwo))
 #print(float_closes)
 
+'''Finding the daily changes between each day (14 changes)'''
 daily_changes_listone = []
 for i in range(1,len(float_closes)):
     x = float_closes[i] - float_closes[i-1]
@@ -69,6 +78,7 @@ for change in daily_changes_listtwo:
 print(upward_movementstwo)
 print(downward_movementstwo)
 
+'''Downward movements must be positive in the RSI calculation; turning the negative movements into positive using absolute value'''
 abs_downward_movements = [abs(x) for x in downward_movements]
 abs_downward_movementstwo = [abs(x) for x in downward_movementstwo]
 #print(abs_downward_movements)
@@ -81,10 +91,11 @@ previous_downchange = abs_downward_movements[-1]
 previous_upchangetwo = upward_movementstwo[-1]
 previous_downchangetwo = abs_downward_movementstwo[-1]
 
-
+'''Finding the initial upward movement average for the second most previous day'''
 initial_up_movement = sum(upward_movements)/len(upward_movements)
 print(len(upward_movements))
 
+'''Calculating the up movement avg for the previous day'''
 avg_up_movement = (initial_up_movement*13 + previous_upchangetwo)/14
 
 initial_down_movement = sum(abs_downward_movements)/len(abs_downward_movements)
